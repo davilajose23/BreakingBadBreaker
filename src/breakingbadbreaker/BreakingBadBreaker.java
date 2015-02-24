72a168911b0db7a22c3c8805f9b738a689c31833
Jose Fernando <davila.jose23@gmail.com>
2015-02-24 16:55:10 -0600

    Estructura basica del JFrame

--- src/breakingbadbreaker/BreakingBadBreaker.java
+++ src/breakingbadbreaker/BreakingBadBreaker.java
@@ -5,17 +5,179 @@
  */
 package breakingbadbreaker;
 
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.LinkedList;
+import javax.swing.JFrame;
+
 /**
  *
  * @author JoseFernando
  */
-public class BreakingBadBreaker {
+public class BreakingBadBreaker extends JFrame implements Runnable, KeyListener {
+
+   /**
+     * Constructor de la clase <code>JuegoJFrame</code>.
+     * En este metodo se inizializan las variables o se crean los objetos
+     * a usarse en el <code>JFrame</code> y se definen funcionalidades.
+     */
+	public BreakingBadBreaker() {
+        
+            
+            
+            
+        // Declaras un hilo
+        Thread t = new Thread (this);
+	// Empieza el hilo
+	t.start ();
+            
+        }
+    
+    /** 
+     * Metodo <I>run</I> sobrescrito de la clase <code>Thread</code>.<P>
+     * En este metodo se ejecuta el hilo, es un ciclo indefinido donde se incrementa
+     * la posicion en x o y dependiendo de la direccion, finalmente 
+     * se repinta el <code>JFrame</code> y luego manda a dormir el hilo.
+     * 
+     */
+    @Override
+    public void run() {
+     
+    }
+        
+    
+    
+    /**
+     * Metodo usado para actualizar la posicion de los objetos
+     * 
+     */
+    public void actualiza(){
+            
+            
+            
+    }
+    
+    
+    /**
+     * Metodo usado para checar las colisiones del objeto elefante y raton
+     * con las orillas del <code>Applet</code>.
+     */
+    public void checaColision(){
+        
+        
+        
+        
+        
+    }
 
     /**
-     * @param args the command line arguments
+     * Metodo <I>paint</I> sobrescrito de la clase <code>Applet</code>,
+     * heredado de la clase Container.<P>
+     * En este metodo lo que hace es actualizar el contenedor
+     * En este metodo se dibuja la imagen con la posicion actualizada,
+     * @param graphics es el <code>objeto grafico</code> usado para dibujar.
+     */
+    
+    public void paint(Graphics graphics) {
+        
+        
+        
+    }
+    
+   /**
+     * Metodo <I>paint1</I> sobrescrito de la clase <code>Applet</code>,
+     * heredado de la clase Container.<P>
+     * En este metodo se dibuja la imagen con la posicion actualizada,
+     * ademas que cuando la imagen es cargada te despliega una advertencia.
+     * @param graphics es el <code>objeto grafico</code> usado para dibujar.
+     */
+    
+    public void paint1 (Graphics graphics){
+        
+        
+    }
+    
+    /**
+     * Metodo <I>keyPressed</I> sobrescrito de la interface <code>KeyListener</code>.<P>
+     * En este metodo maneja el evento que se genera al presionar cualquier la tecla.
+     * @param keyevent es el <code>evento</code> generado al presionar las teclas.
+     */
+    @Override
+    public void keyPressed(KeyEvent keyevent) {
+        
+        
+        repaint();
+        
+    }
+    
+    
+    /**
+     * Metodo <I>keyTyped</I> sobrescrito de la interface <code>KeyListener</code>.<P>
+     * En este metodo maneja el evento que se genera al presionar una tecla que no es de accion.
+     * @param keyevent es el <code>evento</code> que se genera en al presionar las teclas.
+     */
+    
+    @Override
+    public void keyTyped(KeyEvent keyevent) {
+        
+    }
+    
+    /**
+     * Metodo <I>keyReleased</I> sobrescrito de la interface <code>KeyListener</code>.<P>
+     * En este metodo maneja el evento que se genera al soltar la tecla presionada.
+     * @param e es el <code>evento</code> que se genera en al soltar las teclas.
+     */
+    @Override
+    public void keyReleased(KeyEvent keyevent) {
+        
+    }
+    /**
+     * Metodo que lee a informacion de un archivo.
+     *
+     * @throws IOException
+     */
+    public void leeArchivo() throws IOException{
+    	
+    }
+	
+    /**
+     * Metodo que escribe en el archivo
+     *
+     * @throws IOException
+     */
+    public void grabaArchivo() throws IOException{
+    	
+        
+        
+    }
+
+    
+    /**
+     * Metodo principal
+     *
+     * @param args es un arreglo de tipo <code>String</code> de linea de comandos
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+    	// TODO code application logic here
+    	
+
     }
+    
+    
     
 }
